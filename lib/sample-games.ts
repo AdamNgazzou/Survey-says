bc46c915aa50b77b5d3008c1a17686cd546f7f23
Adam Ngazzou <117188411+AdamNgazzou@users.noreply.github.com>
2025-09-17 19:56:35 +0100

    Update sample-games.ts

--- lib/sample-games.ts
+++ lib/sample-games.ts
@@ -1,5 +1,92 @@
 export const sampleGames = [
   {
+    id: "Quiz ma3 loled",
+    title: "Quiz ma3 loled",
+    description: "Classic family-friendly questions that everyone can enjoy",
+    questions: [
+      {
+        question: "chneya a5yeb hejet ynjm ykon 3andk 7asasiya menhom",
+        answers: [
+          { text: "mekla", points: 35 },
+          { text: "3bed", points: 25 },
+          { text: "mé", points: 18 },
+          { text: "flous", points: 12 },
+          { text: "7aywanet", points: 10 },
+
+        ],
+      },
+      {
+        question: "7ajet kol ma tekber fil 3mor yonkso andek",
+        answers: [
+          { text: "ch3ar", points: 35 },
+          { text: "energy", points: 25 },
+          { text: "sennin", points: 18 },
+          { text: "virilité", points: 12 },
+          { text: "sabr", points: 10 },
+        ],
+      },
+      {
+        question: "What s something CS students complain about the most?",
+        answers: [
+          { text: "Too many assignments", points: 35 },
+          { text: "Lack of sleep", points: 25 },
+          { text: "Bad Wi-Fi", points: 18 },
+          { text: "Long lectures", points: 10 },
+          { text: "Old/outdated computers", points: 10 },
+        ],
+      },
+      {
+        question: "7ajet yetchakew menhom labed f sbitar",
+        answers: [
+          { text: "el retard", points: 35 },
+          { text: "el mekla", points: 25 },
+          { text: "el flous", points: 18 },
+          { text: "el ri7a", points: 12 },
+          { text: "el service", points: 10 },
+        ],
+      },
+            {
+        question: "7ajet ta3malhom wenti chtar re9ed",
+        answers: [
+          { text: "tetfrj fil tv/serie", points: 35 },
+          { text: "ta9ra", points: 25 },
+          { text: "ta7ki ma3 chkoun", points: 18 },
+          { text: "scrolling", points: 12 },
+        ],
+      },
+        {
+        question: "a5yeb blayes ynjm yjik fehom telifoun",
+        answers: [
+          { text: "wenti tfaski", points: 35 },
+          { text: "entretien ta3 5edma", points: 25 },
+          { text: "toilette", points: 18 },
+          { text: "cinema", points: 12 },
+          { text: "jame3", points: 10 },
+
+        ],
+      },
+      {
+        question: "7ajet les parents ywasiw fik bech to7sen testaamalhom / testghalhom",
+        answers: [
+          { text: "flous", points: 35 },
+          { text: "wa9t", points: 25 },
+          { text: "dhaw", points: 18 },
+          { text: "me", points: 12 },
+
+        ],
+      },
+      {
+        question: "Companies estaghnew ml AI (ma3rofin)",
+        answers: [
+          { text: "NVIDIA", points: 35 },
+          { text: "openAI", points: 25 },
+          { text: "google", points: 18 },
+          { text: "tesla", points: 12 },
+        ],
+      },
+    ],
+  },
+  {
     id: "family-favorites",
     title: "Family Favorites",
     description: "Classic family-friendly questions that everyone can enjoy. Perfect for game night with all ages!",
